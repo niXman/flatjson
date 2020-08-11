a66f7daef30fdd67d45c38ce7b06a30fbcbcf452
niXman <i.nixman@autistici.org>
2020-08-11 17:12:57 +0300

    unused macro was removed

--- flatjson.hpp
+++ flatjson.hpp
@@ -1436,12 +1436,7 @@ private:
 #undef __FLATJSON__KLEN_TYPE
 #undef __FLATJSON__VLEN_TYPE
 #undef __FLATJSON__CHILDS_TYPE
-#undef __FLATJSON__IS_SIMPLE_TYPE
-#undef __FLATJSON__IS_DIGIT
-#undef __FLATJSON__IS_HEX_DIGIT
-#undef __FLATJSON__IS_HEX_DIGIT4
 #undef __FLATJSON__CUR_CHAR
-#undef __FLATJSON__UTF8_CHAR_LEN
 
 /*************************************************************************************************/
 
